0c45c97d75ee8b9b10a670b85e2f25712399f69f
niyatisharma2187-dev <niyatisharma2187@gmail.com>
2026-05-14 14:19:58 +0530

    Update and rename leapyear.html to leapyear.js

--- leapyear.js
+++ leapyear.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <script>
         year=prompt("Enter year");
         if((year%4==0 && year%100!=0) || year%400==0)
@@ -17,5 +10,4 @@
             document.write("It is not a leap year!!");
         }
     </script>
-</body>
-</html>
+
